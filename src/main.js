4b04f06bb10209045e1b6715845fdf72db956a2a
zzkwan <1609165674@qq.com>
2020-11-28 19:26:02 +0800

    初始化提交

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import './styles/index.less'
+
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+//创建Vue根实例
+createApp(App).use(store).use(router).use(Vant).mount('#app')
